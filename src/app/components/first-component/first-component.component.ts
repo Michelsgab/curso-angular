c42d33c502b20244427e2df6b12d540c7e728d87
Michelsgab <eugabmichels@gmail.com>
2022-06-27 16:15:34 -0300

    Firsts Steps with Angular

---
+++ src/app/components/first-component/first-component.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css'],
+})
+export class FirstComponentComponent implements OnInit {
+  name = 'Gabriel';
+  age = 18;
+  dream = ['Helping others', 'Family', 'Audi'];
+  car = {
+    make: 'Audi',
+    model: 'TTRS',
+    color: 'Blue',
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
